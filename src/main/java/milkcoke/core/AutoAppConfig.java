48439a11e2ce4ec97b6d18e875e4b77d8c7e1b10
milkcoke <mbh023@konkuk.ac.kr>
2022-11-30 18:30:39 +0900

    test: Add same bean name conflict test code

--- src/main/java/milkcoke/core/AutoAppConfig.java
+++ src/main/java/milkcoke/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package milkcoke.core;
 
+import milkcoke.core.repository.member.MemberRepository;
+import milkcoke.core.repository.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,4 +17,12 @@ import org.springframework.context.annotation.FilterType;
 public class AutoAppConfig {
     // DI 를 생성자 없이 어떻게 할 것인가? => AutoWired!
 
+    // Overriding bean definition for bean 'memoryMemberRepository' with a different definition
+    // : replacing [Generic bean: class [milkcoke.core.repository.member.MemoryMemberRepository]
+    // 수동 등록 빈이 자동 등록 빈에 비해 우선권을 가지고 오버라이딩 해준다.
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memoryRepository() {
+        return new MemoryMemberRepository();
+    }
+    // 비록 정상 실행되더라도, Bean 이름을 달리하여 중복 충돌을 회피하는 것이 좋다.
 }
