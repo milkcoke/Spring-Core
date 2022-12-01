b22c785f397525fcee1e6c6a9c427e7b8ce4a136
milkcoke <mbh023@gmail.com>
2022-12-01 15:55:15 +0900

    chore: Update typo in MemberApp.java

--- src/main/java/milkcoke/core/MemberApp.java
+++ src/main/java/milkcoke/core/MemberApp.java
@@ -3,7 +3,6 @@ package milkcoke.core;
 import milkcoke.core.domain.member.Grade;
 import milkcoke.core.domain.member.Member;
 import milkcoke.core.service.member.MemberService;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
@@ -11,7 +10,7 @@ public class MemberApp {
 
         // 모든 객체 Bean 을 관리함
         // Spring Container 에 집어 넣어 관리함.
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         // 메서드 이름으로 name 이 설정되어있음. (key: 메소드명, value: 생성된 객체)
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
