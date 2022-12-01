ddbfc9921efa52b1ee9a588ee842b29777820cc6
milkcoke <mbh023@gmail.com>
2022-12-01 20:53:10 +0900

    build: Add lombok `@RequiredArgsConstructor` annotation
    Use this for Constructor DI.

--- src/main/java/milkcoke/core/service/order/OrderServiceImpl.java
+++ src/main/java/milkcoke/core/service/order/OrderServiceImpl.java
@@ -1,6 +1,9 @@
 package milkcoke.core.service.order;
 
+import lombok.RequiredArgsConstructor;
+import milkcoke.core.annotation.MainDiscountPolicy;
 import milkcoke.core.domain.discount.DiscountPolicy;
+import milkcoke.core.domain.member.Member;
 import milkcoke.core.domain.order.Order;
 import milkcoke.core.repository.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +11,13 @@ import org.springframework.stereotype.Service;
 
 // order service 는 할인에 대한 것을 아무것도 알지 몬하는 상태다.
 @Service
+// final 키워드가 붙은 모든 프로퍼티에 대한 생성자를 만들어준다.
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     // 구현체가 아닌 Interface (역할) 에만 의존해야한다. => DIP 원칙 준수
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // 구현객체를 대신 생성하여 '주입'해줘야한다.
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
